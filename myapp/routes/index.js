d7b68122fd64556afbe5db92508986c8ba1fc0c4
Gabriel FERREIRA <gferreir@c4r3s4.wethinkcode.co.za>
2019-05-30 17:25:15 +0200

    passport in index.js

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const app = express();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -23,5 +24,55 @@ router.get('/updateprofile', function(req, res, next) {
 router.get('/photos', function(req, res, next) {
   res.render('photos');
 });
+
+/*  PASSPORT SETUP  */
+
+const passport = require('passport');
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/success', (req, res) => res.send("Welcome "+req.query.username+"!!"));
+app.get('/error', (req, res) => res.send("error logging in"));
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user.id);
+});
+
+passport.deserializeUser(function(id, cb) {
+  User.findById(id, function(err, user) {
+    cb(err, user);
+  });
+});
+
+/* PASSPORT LOCAL AUTHENTICATION */
+
+const LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+      UserDetails.findOne({
+        username: username
+      }, function(err, user) {
+        if (err) {
+          return done(err);
+        }
+
+        if (!user) {
+          return done(null, false);
+        }
+
+        if (user.password != password) {
+          return done(null, false);
+        }
+        return done(null, user);
+      });
+  }
+));
+
+app.post('/',
+  passport.authenticate('local', { failureRedirect: '/error' }),
+  function(req, res) {
+    res.redirect('/success?username='+req.user.username);
+  });
 
 module.exports = router;
